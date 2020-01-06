b1cdbcf36dde71ea9c626489072db0d345313aac
Arvin <Arvinxl@163.com>
2020-01-06 22:32:12 +0800

    commit by lijian

---
+++ api-gateway/src/main/java/com/blingcavalry/zuulserver/ZuulServerAplication.java
@@ -0,0 +1,20 @@
+package com.blingcavalry.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @auhther Arvin
+ * @date 2020/1/6 22:25
+ * @description:
+ */
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServerAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerAplication.class, args);
+    }
+}
---
+++ test-eureka/src/main/java/com/blingcavalry/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.blingcavalry.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auhther Arvin
+ * @date 2020/1/6 22:22
+ * @description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
