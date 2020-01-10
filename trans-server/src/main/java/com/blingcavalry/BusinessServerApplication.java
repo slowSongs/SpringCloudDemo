8a8dbaf35dd9d7eff0fee461dfe9218eb8689fd9
jinfeng.wang <jinfeng.wang@meditrusthealth.com>
2020-01-10 09:35:40 +0800

    配置服务

--- trans-server/src/main/java/com/blingcavalry/BusinessServerApplication.java
+++ trans-server/src/main/java/com/blingcavalry/BusinessServerApplication.java
@@ -1,4 +1,13 @@
 package com.blingcavalry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
 public class BusinessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessServerApplication.class);
+    }
 }
--- trans-server/src/main/java/com/blingcavalry/controller/ConfigClientController.java
+++ trans-server/src/main/java/com/blingcavalry/controller/ConfigClientController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/config")
+@RestController
+@RequestMapping("/config")
 public class ConfigClientController {
 
     @Value("${profile}")
